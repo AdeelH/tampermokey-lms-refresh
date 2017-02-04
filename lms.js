d41d7629427cf64667a978f43a2155ecdbd4efb3
Adeel <adeel_hassan93@hotmail.com>
2017-02-04 20:14:43 +0500

    streamline code

--- lms.js
+++ lms.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IBA-LMS session refresher
 // @namespace    https://github.com/AdeelH/
-// @version      0.1
+// @version      0.2
 // @description  Prevent login timeout
 // @author       Adeel Hassan
 // @match        *://*.iba.edu.pk/portal/*
@@ -9,28 +9,15 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-
 	if (!/^lms2?\.iba\.edu\.pk/i.test(location.hostname)) {
 		return;
 	}
-    setTimeout(tamper, 10*1000);
+    setInterval(refreshSession, 1 * 60 * 1000);
 })();
 
-function tamper() {
-    window.refreshSession = function() {
-		if (typeof(jQuery) === 'undefined')
-			return;
-        jQuery.ajax({
-            url: '/direct/session/current.json?_=' + (new Date()).getTime(),
-            dataType: 'json',
-            success: function(data){
-                setTimeout(window.refreshSession, 1000 * 60 * 1);
-            },
-            error: function(err) {
-                console.log('err', err);
-            }
-        });
-    };
-    window.refreshSession();
+function refreshSession() {
+    return jQuery && jQuery.ajax({
+        url: '/direct/session/current.json?_=' + Date.now(),
+        dataType: 'json',
+    });
 }
